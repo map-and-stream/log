0357096313dfd2f7ffeff6d89d56df7c4ed82741
alojavad <javad.hajian@hotmail.com>
2025-10-23 16:05:23 +0330

    remove extra comment

--- test/test_log.cpp
+++ test/test_log.cpp
@@ -19,17 +19,3 @@ TEST(LogTest, InfoOutput) {
     std::string output = testing::internal::GetCapturedStdout();
     EXPECT_NE(output.find("[INFO] Hello"), std::string::npos);
 }
-
-// TEST(LogTest, WarnOutput) {
-//     testing::internal::CaptureStdout();
-//     log::warn("Warning");
-//     std::string output = testing::internal::GetCapturedStdout();
-//     EXPECT_NE(output.find("[WARN] Warning"), std::string::npos);
-// }
-
-// TEST(LogTest, ErrorOutput) {
-//     testing::internal::CaptureStderr();
-//     log::error("Error");
-//     std::string output = testing::internal::GetCapturedStderr();
-//     EXPECT_NE(output.find("[ERROR] Error"), std::string::npos);
-// }
